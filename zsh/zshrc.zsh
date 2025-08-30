415442264c2e7bf702f42d37f90be917f75d901a
Paulo Victor <paulovictordev16@gmail.com>
2025-08-30 14:40:57 -0300

    feat: add new alias and zsh theme

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,7 +1,19 @@
-export ZSH="$HOME/.oh-my-zsh"
-# THEME
-ZSH_THEME="amuse"
+# ZSH Theme
+ZSH_THEME="af-magic"
+
+# Aliases 
+alias zshconfig="nvim ~/.zshrc"
+alias la="ls -la"
+alias nrd="npm run dev"
+
+# History
+HISTSIZE=10000
+HISTFILESIZE=20000
+
 # PLUGINS
 plugins=(git)
-# SOURCE
+
+# Others
 source $ZSH/oh-my-zsh.sh
+
+export ZSH="$HOME/.oh-my-zsh"
