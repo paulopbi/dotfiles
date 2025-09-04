af6b13c9ad2573c87308061bfc832181f2c3ab6f
Paulo Victor <paulovictordev16@gmail.com>
2025-09-04 20:50:25 -0300

    chore: remove the alias variables

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,11 +1,6 @@
 # ZSH Theme
 ZSH_THEME="af-magic"
 
-# Aliases 
-alias zshconfig="nvim ~/.zshrc"
-alias la="ls -la"
-alias nrd="npm run dev"
-
 # History
 HISTSIZE=10000
 HISTFILESIZE=20000
@@ -15,5 +10,4 @@ plugins=(git)
 
 # Others
 source $ZSH/oh-my-zsh.sh
-
 export ZSH="$HOME/.oh-my-zsh"
