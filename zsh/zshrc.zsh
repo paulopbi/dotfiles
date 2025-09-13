382a3a9ad04d38cb64ca387cf73236e30bccdfc8
Paulo Victor <paulovictordev16@gmail.com>
2025-09-13 13:37:18 -0300

    feat: add new plugins and case sensitive config

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,13 +1,19 @@
 # ZSH Theme
 ZSH_THEME="af-magic"
-
 # History
 HISTSIZE=10000
 HISTFILESIZE=20000
 
 # PLUGINS
-plugins=(git)
+plugins=(
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+  you-should-use
+  zsh-bat
+  git
+)
 
 # Others
+CASE_SENSITIVE="true"
 source $ZSH/oh-my-zsh.sh
 export ZSH="$HOME/.oh-my-zsh"
