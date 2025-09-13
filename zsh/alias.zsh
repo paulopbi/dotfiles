932a1cce4d3d6d3c36dda168436e62e353ae2165
Paulo Victor <paulovictordev16@gmail.com>
2025-09-13 13:36:46 -0300

    feat: add new alias and custom functions

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,3 +1,13 @@
+# alias
+alias bashconfig="nano ~/.bashrc"
 alias zshconfig="nvim ~/.zshrc"
 alias la="ls -la"
 alias nrd="npm run dev"
+alias cls="clear"
+alias explorer="xdg-open ."
+alias update="sudo dnf update -y"
+
+# custom functions 
+mkcd() {
+  mkdir -p "$1" && cd "$1"
+}
